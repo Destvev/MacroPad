0871deb03deaa12a7f4eea3b9a843382770f11e4
Tomasz Czyrek <destvev62@gmail.com>
2026-05-16 20:16:34 +0200

    Add matrix configuration and Vial settings to config.h

--- config.h
+++ config.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 3
+
+// Twoje fizyczne połączenia bezpośrednie do masy
+#define DIRECT_PINS { \
+    { GP15, GP14, GP13 }, \
+    { GP26, GP27, GP22 }, \
+    { GP6, GP7, GP8 } \
+}
+
+// Unikalny identyfikator dla Viala
+#define VIAL_KEYBOARD_UID {0x42, 0x69, 0x33, 0x37, 0x24, 0x99, 0x15, 0x01}
+#define VIAL_UNLOCK_COMBO {0, 0}, {0, 1}
